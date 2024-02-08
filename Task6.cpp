21157cd81c812bb1d84f06ee342e3412181d4c1a
GannaElkhen <158619932+GannaElkhen@users.noreply.github.com>
2024-02-08 23:53:04 +0200

    Add files via upload

---
+++ Task6.cpp
@@ -0,0 +1,250 @@
+#include <iostream> 
+using namespace std ;
+int main(){
+int n;
+cout<<"Enter a number from 2 to 10";
+cin>>n;
+if(n==2){
+int x1,x2,max;
+cout<<"Enter 2 numbers";
+cin>>x1>>x2;
+if(x1>x2)
+max=x1;
+else
+max=x2;
+cout<<"The largest number is "<<max;
+}
+else if(n==3){
+int x1,x2,x3,max;
+cout<<"Enter 3 numbers";
+cin>>x1>>x2>>x3;
+if(x1>x2)
+max=x1;
+else 
+max=x2;
+if(max>x3)
+    max=max;
+    else 
+    max=x3;
+cout<<"The largest number is "<<max;
+}
+else if(n==4){
+int x1,x2,x3,x4,max;
+cout<<"Enter 4 numbers";
+cin>>x1>>x2>>x3>>x4;
+if(x1>x2)
+max=x1;
+else 
+max=x2;
+if(max>x3)
+    max=max;
+    else 
+    max=x3;
+    if(max>x4)
+    max=max;
+    else
+    max=x4;
+cout<<"The largest number is "<<max;
+}
+else if(n==5){
+int x1,x2,x3,x4,x5,max;
+cout<<"Enter 5 numbers";
+cin>>x1>>x2>>x3>>x4>>x5;
+if(x1>x2)
+max=x1;
+else 
+max=x2;
+if(max>x3)
+    max=max;
+    else 
+    max=x3;
+    if(max>x4)
+    max=max;
+    else 
+    max=x4;
+    if(max>x5)
+    max=max;
+    else 
+    max=x5;
+cout<<"The largest number is "<<max;}
+else if(n==6){
+int x1,x2,x3,x4,x5,x6,max;
+cout<<"Enter 6 numbers";
+cin>>x1>>x2>>x3>>x4>>x5>>x6;
+if(x1>x2)
+max=x1;
+else 
+max=x2;
+if(max>x3)
+    max=max;
+    else 
+    max=x3;
+
+    if(max>x4)
+    max=max;
+    else 
+    max=x4;
+    if(max>x5)
+    max=max;
+    else 
+    max=x5;
+    if(max>x6)
+    max=max;
+    else
+    max=x6;
+cout<<"The largest number is "<<max;}
+else if(n==7){
+int x1,x2,x3,x4,x5,x6,x7,max;
+cout<<"Enter 7 numbers";
+cin>>x1>>x2>>x3>>x4>>x5>>x6>>x7;
+if(x1>x2)
+max=x1;
+else 
+max=x2;
+if(max>x3)
+    max=max;
+    else 
+    max=x3;
+
+    if(max>x4)
+    max=max;
+    else 
+    max=x4;
+    if(max>x5)
+    max=max;
+    else 
+    max=x5;
+    if(max>x6)
+    max=max;
+    else
+    max=x6;
+    if(max>x7)
+    max=max;
+    else
+    max=x7;
+cout<<"The largest number is "<<max;}
+else if(n==8){
+int x1,x2,x3,x4,x5,x6,x7,x8,max;
+cout<<"Enter 8 numbers";
+cin>>x1>>x2>>x3>>x4>>x5>>x6>>x7>>x8;
+if(x1>x2)
+max=x1;
+else 
+max=x2;
+if(max>x3)
+    max=max;
+    else 
+    max=x3;
+    if(max>x4)
+    max=max;
+    else 
+    max=x4;
+    if(max>x5)
+    max=max;
+    else 
+    max=x5;
+    if(max>x6)
+    max=max;
+    else
+    max=x6;
+    if(max>x7)
+    max=max;
+    else
+    max=x7;
+    if(max>x8)
+    max=max;
+    else
+    max=x8;
+cout<<"The largest number is "<<max;}
+else if(n==9){
+int x1,x2,x3,x4,x5,x6,x7,x8,x9,max;
+cout<<"Enter 9 numbers";
+cin>>x1>>x2>>x3>>x4>>x5>>x6>>x7>>x8>>x9;
+if(x1>x2)
+max=x1;
+else 
+max=x2;
+if(max>x3)
+    max=max;
+    else 
+    max=x3;
+    if(max>x4)
+    max=max;
+    else 
+    max=x4;
+    if(max>x5)
+    max=max;
+    else 
+    max=x5;
+    if(max>x6)
+    max=max;
+    else
+    max=x6;
+    if(max>x7)
+    max=max;
+    else
+    max=x7;
+    if(max>x8)
+    max=max;
+    else
+    max=x8;
+    if(max>x9)
+    max=max;
+    else
+    max=x9;
+cout<<"The largest number is "<<max;}
+else if(n==10){
+int x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,max;
+cout<<"Enter 10 numbers";
+cin>>x1>>x2>>x3>>x4>>x5>>x6>>x7>>x8>>x9>>x10;
+if(x1>x2)
+max=x1;
+else 
+max=x2;
+if(max>x3)
+    max=max;
+    else 
+    max=x3;
+    if(max>x4)
+    max=max;
+    else 
+    max=x4;
+    if(max>x5)
+    max=max;
+    else 
+    max=x5;
+    if(max>x6)
+    max=max;
+    else
+    max=x6;
+    if(max>x7)
+    max=max;
+    else
+    max=x7;
+    if(max>x8)
+    max=max;
+    else
+    max=x8;
+    if(max>x9)
+    max=max;
+    else
+    max=x9;
+if(max>x10)
+max=max;
+else
+max=x10;
+cout<<"The largest number is "<<max;}
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+
+}
